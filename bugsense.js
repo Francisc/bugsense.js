89aed31466947157ebb894631495d72282633b18
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2013-04-19 16:05:38 +0300

    Changes encoding to UTF-8

--- bugsense.js
+++ bugsense.js
@@ -44,15 +44,7 @@ var bugsense;
       }
       return result;
     }
-  })();
-
-  if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this)
-                action.call(that, this[i], i, this);
-    };
-  }
+}( this, function () { 
 
   /**
    * Simple extend() implementation
