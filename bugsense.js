01f091282d1964279440ca4f8d955e9cba2d9106
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2014-01-17 12:59:10 +0200

    Adds userIdentifier in the crash reports

--- bugsense.js
+++ bugsense.js
@@ -2037,6 +2037,7 @@ extend(Bugsense.prototype, {
         'version' : '2.0'
       },
       request: {
+        'user_id': ( this.config.userIdentifier || 'unknown' ),
         'custom_data' : []
       },
       exception: {
